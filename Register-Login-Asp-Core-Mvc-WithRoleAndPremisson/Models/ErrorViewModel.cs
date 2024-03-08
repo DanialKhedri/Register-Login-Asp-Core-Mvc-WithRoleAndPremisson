9459f09f2303e1783e207f166fe849ca2d9204f2
Danial Khedri <Danial.kh79@gmail.com>
2024-03-08 22:33:55 +0330

    Add project files.

---
+++ Register-Login-Asp-Core-Mvc-WithRoleAndPremisson/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Register_Login_Asp_Core_Mvc_WithRoleAndPremisson.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
