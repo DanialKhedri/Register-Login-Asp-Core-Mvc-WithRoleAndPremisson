51fc073632ba88b4f666b583ed6d28573ae6d362
Danial Khedri <Danial.kh79@gmail.com>
2024-03-12 04:24:02 +0330

    befor migration1

---
+++ Infrastructure/Data/DataContext.cs
@@ -0,0 +1,41 @@
+﻿using Domain.Entities.Role;
+using Domain.Entities.SelectedRole;
+using Domain.Entities.User;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class DataContext : DbContext
+    {
+        #region OnConfig
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer(@$"Data Source=(LocalDB)\MSSQLLocalDB;Integrated Security=True;Connect Timeout=30;TrustServerCertificate=True");
+        }
+
+        #endregion
+
+
+
+
+        #region Dbsets
+        DbSet<User> Users { get; set; }
+
+        DbSet<Role> Roles { get; set; }
+
+        DbSet<SelectedRole> SelectedRoles { get; set; }
+
+        #endregion
+
+
+
+    }
+}
