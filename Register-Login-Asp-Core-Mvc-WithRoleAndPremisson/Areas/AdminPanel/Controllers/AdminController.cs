258922e6382a9cea226f0549a7039a0c7ed3cb74
Danial Khedri <Danial.kh79@gmail.com>
2024-03-13 14:07:47 +0330

    AdminPanel Controller and View Added

---
+++ Register-Login-Asp-Core-Mvc-WithRoleAndPremisson/Areas/AdminPanel/Controllers/AdminController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Register_Login_Asp_Core_Mvc_WithRoleAndPremisson.Areas.AdminPanel.Controllers
+{
+    public class AdminController : Controller
+    {
+        [Area("AdminPanel")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
