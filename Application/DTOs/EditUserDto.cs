e998a94d64ca4516d37507c1b2acbec52ce86d2b
Danial Khedri <Danial.kh79@gmail.com>
2024-03-16 20:55:58 +0330

    EditUSerDto Added

---
+++ Application/DTOs/EditUserDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs
+{
+    public class EditUserDto
+    {
+        #region Properties
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public DateTime CreateDate { get; set; } = DateTime.Now;
+
+        public string? UserAvatar { get; set; } = null;
+
+        List<int> UserSelectedRoles { get; set; }
+
+        #endregion
+    }
+}
