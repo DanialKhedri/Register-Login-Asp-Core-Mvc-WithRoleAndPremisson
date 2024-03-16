bfa8f4096c1552f22d18285b1a2654650502077d
Danial Khedri <Danial.kh79@gmail.com>
2024-03-16 16:55:59 +0330

    ListOfUsers View Added

--- Register-Login-Asp-Core-Mvc-WithRoleAndPremisson/Areas/AdminPanel/Controllers/AdminController.cs
+++ Register-Login-Asp-Core-Mvc-WithRoleAndPremisson/Areas/AdminPanel/Controllers/AdminController.cs
@@ -5,62 +5,59 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Register_Login_Asp_Core_Mvc_WithRoleAndPremisson.Areas.ActionFilterAttributes;
 
-namespace Register_Login_Asp_Core_Mvc_WithRoleAndPremisson.Areas.AdminPanel.Controllers
+namespace Register_Login_Asp_Core_Mvc_WithRoleAndPremisson.Areas.AdminPanel.Controllers;
+
+[Area("AdminPanel")]
+[Authorize]
+[CheckUserIsAdmin]
+public class AdminController : Controller
 {
-    [Area("AdminPanel")]
-    [Authorize]
-    [CheckUserIsAdmin]
-    public class AdminController : Controller
+
+    #region Ctor
+
+    private readonly IUserService _IuserService;
+
+    public AdminController(IUserService IuserService)
     {
+        _IuserService = IuserService;
+    }
 
-        #region Ctor
+    #endregion
 
-        private readonly IUserService _IuserService;
+    #region Index
 
-        public AdminController(IUserService IuserService)
-        {
-            _IuserService = IuserService;
-        }
+    public async Task<IActionResult> Index()
+    {
+        return View();
+    }
 
-        #endregion
-
-        #region Index
-
-        public async Task<IActionResult> Index()
-        {
-            return View();
-        }
-
-        #endregion
+    #endregion
 
 
 
-        #region ListOfUsers
-
-        [HttpGet, ValidateAntiForgeryToken]
-        public async Task<IActionResult> ListOfUsers()
-        {
-
-            return View();
-
-        }
-
-        #endregion
-
-
-
-
-
-        #region EditUser
-        [HttpGet, ValidateAntiForgeryToken]
-        public async Task<IActionResult> EditUser()
-        {
-            return View();
-        }
-        #endregion
-
-
-
+    #region ListOfUsers
+    [HttpGet]
+    public async Task<IActionResult> ListOfUsers()
+    {
+        return View();
 
     }
+
+    #endregion
+
+
+
+
+
+    #region EditUser
+    [HttpGet, ValidateAntiForgeryToken]
+    public async Task<IActionResult> EditUser()
+    {
+        return View();
+    }
+    #endregion
+
+
+
+
 }
